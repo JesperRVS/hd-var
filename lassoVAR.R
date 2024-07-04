c9b991c2f5837a7e2b7ce07504e5792519ffd42d
JesperRVS <jrvs@econ.ku.dk>
2024-07-04 17:59:43 +0200

    lassoVAR - penalty loadings updating added

--- lassoVAR.R
+++ lassoVAR.R
@@ -3,7 +3,140 @@
 # Dependencies
 library("glmnet", "MASS")
 
-# Multiple-equation weighted LASSO
+lasso <- function(data, q = 1, post = TRUE, intercept = TRUE,
+                      c = 1.1, gamma = 0.1 / log(max(dim(data))), k = 15,
+                      tol_ups = 1e-3, tol_glmnet = 1e-4, warn = TRUE) {
+  if (missing(data) || is.null(data)) {
+    stop("No data provided.")
+  }
+  # INITIALIZE
+  # Unpack data and construct predictors and response
+  nplusq <- nrow(data)  # n + q
+  p <- ncol(data)       # p = dim of output
+  n <- nplusq - q       # n = effective sample size
+  pq <- p*q             # pq = total number of variables
+  y <- data[(q + 1):(q + n), ] # response
+  x <- matrix(NA, nrow = n, ncol = pq) # predictors
+  for (ell in 1:q) {
+    # where to store ellth lag --v
+    block_ell <- ((ell - 1) * p + 1):(ell * p)
+    # periods corresponding to ellth lag --v
+    lag_ell <- (q + 1 - ell):(q + n - ell)
+    # store 1st lags first, 2nd second,... --v
+    x[, block_ell] <- data[lag_ell, ]
+  }
+  # Penalty level
+  lambda_star <- 2 * c * sqrt(n) * qnorm(1 - gamma / (2 * q * p^2))
+  lambda_glmnet <- lambda_star / (2 * n) # penalty in eyes of glmnet
+  # Note: glmnet defines "lambda" based on *half* of *average* square loss,
+  # while our "lambda" stems from *sum* square loss (w/o the 1/2)
+  #  If intercepts requested, demean before proceeding
+  if (intercept == TRUE) {
+    ybar <- colMeans(y)     # response means as p-dim array
+    xbar <- colMeans(x)     # predictor means as pq-dim array
+    y <- sweep(y, 2, ybar)  # demean response
+    x <- sweep(x, 2, xbar)  # demean predictors
+    ybar <- as.matrix(ybar) # response means as p x 1 matrix
+    xbar <- as.matrix(xbar) # predictor means as pq x 1 matrix
+  }
+  # Note: Means are stored to back out intercepts later
+  # ESTIMATION
+  # INITIAL STEP (k=0)
+  ups_init <- sqrt((1 / n) * crossprod(y^2, x^2)) # initial penalty loadings
+  that_init <- mult_lasso(x, y, lambda_glmnet, ups_init, tol_glmnet) # and estimates
+  if (post == TRUE) {                       # if refitting requested...
+    refit <- mult_refit(x, y, that_init)    # refit initial estimates
+    that_init <- refit$that                 # overwrite (keeping zeros)
+    full_rank_post_init <- refit$full_rank  # flag full rank
+  } else { # if no refitting requested...
+    full_rank_post_init <- NULL # then rank deficiency is irrelevant
+  }
+  if (intercept == TRUE) { # if intercepts requested...
+    intr_init <- ybar - that_init %*% xbar # back them out
+  } else{
+    intr_init <- NULL # o/w don't
+  }
+  # UPDATING (UP TO K TIMES)
+  # Create placeholders
+  ups_refi <- array(NA, dim = c(p, pq, k))  # refined penalty loadings
+  that_refi <- array(NA, dim = c(p, pq, k)) # and estimates
+  rel_diffs_ups <- matrix(NA, k, 1)         # relative changes in penalty loadings
+  if (post == TRUE) { # if refitting requested...
+    full_rank_post_refi <- matrix(NA, p, k) # flag full rank
+  } else { # o/w don't
+    full_rank_post_refi <- NULL
+  }
+  if (intercept == TRUE) { # if intercepts requested...
+    intr_refi <- matrix(NA, p, k) # create placeholder
+  } else {
+    intr_refi <- NULL # o/w don't
+  }
+  for (l in 1:k) {                      # start updating
+    if (l == 1) {                       # if first iteration...
+      that_old <- that_init             # use initial estimates
+    } else {                            
+      that_old <- that_refi[, , l - 1]  # o/w use previous (refined) estimates
+    }
+    res_old <- y - x %*% t(that_old) # implied residuals
+    ups_new <- sqrt((1 / n) * t(res_old^2) %*% x^2) # update penalty loadings
+    ups_refi[, , l] <- ups_new # store
+    that_new <- mult_lasso(x, y, lambda_glmnet, ups_new, tol_glmnet) # update estimates
+    if (post == TRUE) {                           # if refitting requested...
+      refit <- mult_refit(x, y, that_new)         # refit estimates
+      that_new <- refit$that                      # overwrite (keeping zeros)
+      full_rank_post_refi[, l] <- refit$full_rank # flag full rank
+    }
+    that_refi[, , l] <- that_new                  # store
+    if (intercept) {                              # if intercepts requested...
+      intr_refi[, l] <- ybar - that_new %*% xbar  # back them out
+    }
+    if (l == 1) {           # if at first update...
+      ups_old <- ups_init   # then compare with initial penalty loadings
+    } else {                # o/w compare with previous (refined) penalty loadings
+      ups_old <- ups_refi[, , l - 1]
+    }
+    diff_ups <- ups_new - ups_old # change in penalty loadings
+    rel_diff_ups <- sqrt(sum(diff_ups^2)) /
+      (.Machine$double.eps + sqrt(sum(ups_old^2))) # relative change
+    rel_diffs_ups[l, 1] <- rel_diff_ups # store
+    if (rel_diff_ups <= tol_ups) {  # if change is small enough...
+      break                         # stop updating
+    }
+  }
+  ups <- ups_refi[, , l]    # report final penalty loadings  
+  if (intercept == TRUE) {  # if intercepts requested...
+    intr <- intr_refi[, l]  # report final intercepts
+  } else {                  
+    intr <- NULL            # o/w don't
+  }
+  that <- that_refi[, , l]  # report final estimates
+  if (warn == TRUE && l == k && rel_diff_ups > tol_ups) {
+    warning("Maximum number of updates reached. Consider increasing K.")
+    cat(sprintf("Relative change in penalty loadings is %3.1g percent\n", 100 * rel_diff_ups))
+  }
+  fit <- list(
+    lambda = lambda_star,                             # penalty level
+    ups_init = ups_init,                              # initial penalty loadings
+    ups_refi = ups_refi[, , 1:l],                     # refined penalty loadings path
+    ups = ups,                                        # final penalty loadings
+    intr_init = intr_init,                            # initial intercepts
+    intr_refi = intr_refi[, 1:l],                     # refined intercepts path 
+    intr = intr,                                      # final intercepts
+    that_init = that_init,                            # initial estimates
+    that_refi = that_refi[, , 1:l],                   # refined estimates path
+    that = that,                                      # final estimates
+    full_rank_post_init = full_rank_post_init,        # flag full rank post-initial
+    full_rank_post_refi = full_rank_post_refi[, 1:l], # flag full rank post-refined path
+    k_term = l,                                       # number of loading updates performed
+    rel_diff_ups_term = rel_diff_ups,                 # relative change in penalty loadings at termination
+    rel_diffs_ups = rel_diffs_ups[1:l, 1]             # relative changes in penalty loadings path
+    )
+  return(fit)
+}
+
+# HELPER FUNCTIONS
+
+# Multiple responses weighted LASSO using same regressors
 mult_lasso <- function(x, y, lambda_glmnet, upsilon = NULL, tol_glmnet = 1e-4) {
   if (missing(x) || missing(y) || missing(lambda_glmnet)) {
     stop("Not enough input arguments.")
@@ -33,144 +166,31 @@ mult_lasso <- function(x, y, lambda_glmnet, upsilon = NULL, tol_glmnet = 1e-4) {
   return(that)
 }
 
-lasso <- function(data, q = 1, post = TRUE, intr = TRUE,
-                      c = 1.1, gamma = 0.1 / log(max(dim(data))), k = 15,
-                      tol_ups = 1e-3, tol_glmnet = 1e-4, warn = TRUE) {
-  if (missing(data) || is.null(data)) {
-    stop("No data provided.")
+# Find solution to minimize ||y - Xb||_{\ell_2} over b
+ls_sol <- function(x, y) {
+  full_rank <- ncol(x) == rankMatrix(x)[1] # check for full column rank
+  if (full_rank == TRUE) { # if full rank, produce unique (OLS) solution  
+    sol <- solve(crossprod(x), crossprod(x, y))
+  } else { # o/w produce solution based on pseudoinverse
+    sol <- MASS::ginv(x) %*% y # ginv is Moore-Penrose pseudoinverse
   }
-  # Unpack data
-  nplusq <- nrow(data)  # n + q
-  p <- ncol(data)       # p = dim of output
-  n <- nplusq - q       # n = effective sample size
-  pq <- p*q             # pq = total number of variables
-  y <- data[(q + 1):(q + n), ] # response
-  x <- matrix(NA, nrow = n, ncol = pq) # predictors
-  for (ell in 1:q) {
-    # where to store ellth lag --v
-    block_ell <- ((ell - 1) * p + 1):(ell * p)
-    # periods corresponding to ellth lag --v
-    lag_ell <- (q + 1 - ell):(q + n - ell)
-    # store 1st lags first, 2nd second,... --v
-    x[, block_ell] <- data[lag_ell, ]
-  }
-  # Penalty level
-  lambda_star <- 2 * c * sqrt(n) * qnorm(1 - gamma / (2 * q * p^2))
-  lambda_glmnet <- lambda_star / (2 * n) # penalty in eyes of glmnet
-  # Note: glmnet defines "lambda" based on *half* of *average* square loss,
-  # while our "lambda" stems from *sum* square loss (w/o the 1/2)
-  #  If intercepts requested, demean before proceeding
-  if (intr == TRUE) {
-    ybar <- colMeans(y)     # means as p-dim array
-    xbar <- colMeans(x)     # means as pq-dim array
-    y <- sweep(y, 2, ybar)  # demean response
-    x <- sweep(x, 2, xbar)  # demean predictors
-    ybar <- as.matrix(ybar) # means as p x 1 matrix
-    xbar <- as.matrix(xbar) # means as pq x 1 matrix
-  }
-  # Note: Means are stored to back out intercepts later
-  # ESTIMATION
-  # Initial step
-  ups_init <- sqrt((1 / n) * crossprod(y^2, x^2)) # initial penalty loadings
-  that_init <- mult_lasso(x, y, lambda_glmnet, ups_init, tol_glmnet) # and estimates
-  if (post == TRUE) {                       # if refitting requested...
-    refit <- mult_refit(x, y, that_init)    # refit initial estimates
-    that_init <- refit$that                 # overwrite (keeping zeros)
-    full_rank_post_init <- refit$full_rank  # check full rank
-  } else { # if no refitting requested...
-    full_rank_post_init <- NULL # then rank deficiency is irrelevant
-  }
-  if (intr == TRUE) { # if intercepts requested...
-    intr_init <- ybar - that_init %*% xbar # back them out
-  } else{
-    intr_init <- NULL # o/w don't
-  }
-  # # Updating
-  # ups_refi <- array(NA, dim = c(p, pq, k))  # refined penalty loadings
-  # that_refi <- array(NA, dim = c(p, pq, k)) # and estimates
-  # if (intr == TRUE) { # if intercepts requested...
-  #   intr_refi <- matrix(NA, p, k) # create placeholder
-  # } else {
-  #   intr_refi <- NULL # o/w don't
-  # }
-  # for (l in 1:k) {
-  #   if (l == 1) {
-  #     that_old <- that_init
-  #   } else {
-  #     that_old <- that_refi[, , l - 1]
-  #   }
-  #   res_old <- y - x %*% t(that_old)
-  #   ups_new <- sqrt((1 / n) * crossprod(res_old^2, x^2))
-  #   ups_refi[, , l] <- ups_new
-  #   that_new <- mult_lasso(x, y, lambda_glmnet, ups_new, tol_glmnet)
-  #   if (post == TRUE) {
-  #     sel <- that_new != 0
-  #     intr_post <- matrix(NA, p, 1)     # p x 1 intercepts
-  #     that_post <- matrix(NA, p, pq)    # p x pq slopes
-  #     full_rank_post_init <- matrix(NA, p, 1) # p x 1 full rank flag
-  #     for (i in 1:p) {
-  #       shat_
-        
-  #     }
-  #   }
-  #   that_refi[, , k] <- that_new
-    
-  #   if (intr) {
-  #     intr_refi[, k] <- Ybar - that_new %*% Xbar
-  #   }
-    
-  #   if (k == 1) {
-  #     UpsOld <- UpsInit
-  #   } else {
-  #     UpsOld <- ups_refi[, , k - 1]
-  #   }
-    
-  #   UpsNew <- ups_refi[, , k]
-  #   dUps <- UpsNew - UpsOld
-  #   reldiffUps <- sqrt(sum(dUps^2)) / sqrt(sum(UpsOld^2))
-    
-  #   if (reldiffUps <= tol_Ups) {
-  #     break
-  #   }
-  # }
-  
-  # Ups <- ups_refi[, , k]
-  # That <- that_refi[, , k]
-  # if (intr) {
-  #   const <- Ybar - That %*% Xbar
-  # } else {
-  #   const <- NULL
-  # }
-  
-  # if (nowarn == 0 && k == K && reldiffUps > tol_Ups) {
-  #   warning("Maximum number of updates reached. Consider increasing K.")
-  #   cat(sprintf("Relative change in penalty loadings is %3.1g percent\n", 100 * reldiffUps))
-  # }
-  fit <- list(
-                lambda = lambda_star,
-                ups_init = ups_init,
-                intr_init = intr_init,
-                that_init = that_init,
-                full_rank_post_init = full_rank_post_init
-                )
+  fit <- list(sol = sol, full_rank = full_rank)
   return(fit)
 }
 
-# Helper functions
-
-# Refit for multiple equations with same regressors
+# Least squares refitting with multiple responses and same regressors
 mult_refit <- function(x, y, that) {
-  n <- nrow(y)                          # num of observations
-  p <- ncol(y)                          # num of equations
+  n <- nrow(y)                          # num observations
+  p <- ncol(y)                          # num responses
   sel <- that != 0                      # flag selections
   full_rank <- matrix(NA, p, 1)         # full rank flag
   for (i in 1:p) {
     shat_i <- sum(sel[i, ])             # num selected
     if (shat_i > 0) {                   # if something selected...
       xi <- as.matrix(x[, sel[i, ]])    # get active regressors
-      refit_i <- ls_sol(xi, y[, i])     # refit
-      that[i, sel[i, ]] <- refit_i$sol  # overwrite (keeping zeros)
-      full_rank[i, 1] <- refit_i$full_rank # check rank
+      fit_i <- ls_sol(xi, y[, i])       # refit
+      that[i, sel[i, ]] <- fit_i$sol    # overwrite (keeping zeros)
+      full_rank[i, 1] <- fit_i$full_rank # check rank
     } else {                            # if nothing selected...
       full_rank[i, 1] <- TRUE           # rank consider "full"
     }
@@ -179,14 +199,4 @@ mult_refit <- function(x, y, that) {
   return(refit)
 }
 
-# Find solution to minimize ||y - Xb||_2 over b
-ls_sol <- function(x, y) {
-  full_rank <- dim(x)[2] == rankMatrix(x)[1] # check for full rank
-  if (full_rank == TRUE) { # if full rank, produce unique solution  
-    sol <- solve(crossprod(x), crossprod(x, y))
-  } else { # o/w produce solution based on pseudoinverse
-    sol <- ginv(x) %*% y # ginv is Moore-Penrose pseudoinverse
-  }
-  fit <- list(sol = sol, full_rank = full_rank)
-  return(fit)
-}
+
--- simulations/testing.R
+++ simulations/testing.R
@@ -4,6 +4,7 @@ invisible(gc()) #free up memory
 
 # Packages
 libest <- c("glmnet")
+libplt <- c("ggplot2") # c("formattable", "ggplot2", "latex2exp", "reshape", "stringr")
 # Auto-installer (checks if installed - if not, installs and loads)
 using <- function(...) {
     libs <- unlist(list(...))
@@ -14,31 +15,55 @@ using <- function(...) {
         lapply(need, require, character.only = TRUE)
     }
 }
-using(libest)
+using(append(libest, libplt))
 
 # Simulate data
 seed <- 2345
 r <- 0
 source("simulations/design_A.R")
-n <- 10
-p <- 12
+n <- 100
+p <- 4
 y0ton <- sim_data(n, p, seed = seed, r = r)
 
 # Estimate
 source("lassoVAR.R")
 q <- 1 # autoregressive order
-fit_lasso <- lasso(y0ton, q = q, post = FALSE, intr = FALSE)
-fit_lasso_intr <- lasso(y0ton, q = q, post = FALSE, intr = TRUE)
-fit_post <- lasso(y0ton, q = q, post = TRUE, intr = FALSE) # HERE HERE HERE
-fit_post_intr <- lasso(y0ton, q = q, post = TRUE, intr = TRUE)
-# fit_post_2 <- lasso(y0ton, q = q, gamma = p, post = TRUE, intr = TRUE)
+fit_lasso <- lasso(y0ton, q = q, post = FALSE, intercept = FALSE)
+refit <- mult_refit(y0ton[1:n, ], y0ton[-1, ], that = fit_lasso$that_init)
 
-fit_ls <- lasso(y0ton, q = q, c = 0, post = FALSE, intr = FALSE)
-fit_ls_intr <- lasso(y0ton, q = q, c = 0, post = FALSE, intr = TRUE)
-fit_ls_post <- lasso(y0ton, q = q, c = 0, post = TRUE, intr = FALSE)
-fit_ls_post_intr <- lasso(y0ton, q = q, c = 0, post = TRUE, intr = TRUE)
+fit_lasso_intr <- lasso(y0ton, q = q, post = FALSE, intercept = TRUE)
+fit_post <- lasso(y0ton, q = q, post = TRUE, intercept = FALSE)
+fit_post_intr <- lasso(y0ton, q = q, post = TRUE, intercept = TRUE)
+# fit_post_2 <- lasso(y0ton, q = q, gamma = p, post = TRUE, intercept = TRUE)
+
+fit_ls <- lasso(y0ton, q = q, c = 0, post = FALSE, intercept = FALSE)
+fit_ls_intr <- lasso(y0ton, q = q, c = 0, post = FALSE, intercept = TRUE)
+fit_ls_post <- lasso(y0ton, q = q, c = 0, post = TRUE, intercept = FALSE)
+fit_ls_post_intr <- lasso(y0ton, q = q, c = 0, post = TRUE, intercept = TRUE)
 
 x <- y0ton[1:n, ]
 y <- y0ton[-1, ]
 
 ybar <- as.matrix(colMeans(y))
+
+a <- c(1:9)
+b <- rev(a)
+
+#array of data
+ab <- array(c(a,b), dim = c(3,3,2))
+
+# Plotting
+
+# Illustrating penalty loading convergence
+plot(1:fit_lasso$k_term, fit_lasso$rel_diffs_ups, type = "l",
+    xlab = "Number of updates, k", ylab = "Relative change in penalty loadings (vectorized ell_2 norm)",
+    main = "LASSO: Relative change in penalty loadings"
+)
+
+
+library(ggplot2)
+ggplot(data = data.frame(x = 1:fit_lasso$k_term, y = fit_lasso$rel_diffs_ups), aes(x = x, y = y)) +
+    geom_line() +
+    labs(x = "Number of updates, k", y = "Relative change in penalty loadings (vectorized ell_2 norm)",
+         title = "LASSO: Relative change in penalty loadings"
+    )
