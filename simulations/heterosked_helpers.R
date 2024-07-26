63e9de5c1757e90c7082f3b3f82b1e980b36709b
JesperRVS <jrvs@econ.ku.dk>
2024-07-26 17:24:33 +0200

    changed design

--- simulations/heterosked_helpers.R
+++ simulations/heterosked_helpers.R
@@ -1,36 +1,47 @@
-# Define the double factorial function
-double_factorial <- function(n) {
-  # Check if the input is a nonnegative integer
-  if (!is.numeric(n) || n < 0 || floor(n) != n) {
-    stop("Input must be a nonnegative integer")
-  }
-  # Handle the base cases
-  if (n == 0 || n == 1) {
-    return(1)
-  }
-  # Initialize the result
-  result <- 1
-  # Calculate the double factorial
-  while (n > 1) {
-    result <- result * n
-    n <- n - 2
-  }
-  return(result)
+# # Define the double factorial function
+# double_factorial <- function(n) {
+#   # Check if the input is a nonnegative integer
+#   if (!is.numeric(n) || n < 0 || floor(n) != n) {
+#     stop("Input must be a nonnegative integer")
+#   }
+#   # Handle the base cases
+#   if (n == 0 || n == 1) {
+#     return(1)
+#   }
+#   # Initialize the result
+#   result <- 1
+#   # Calculate the double factorial
+#   while (n > 1) {
+#     result <- result * n
+#     n <- n - 2
+#   }
+#   return(result)
+# }
+
+# var_m <- function(m = 0) {
+#   if (m == 0) {
+#     return(1)
+#   } else {
+#     return(double_factorial(2 * m - 1) / sqrt(4 * m + 1))
+#   }
+# }
+
+var_m <- function(m) {
+  return(4 * exp(4 * m^2) * pnorm(2 * m) * (1 - pnorm(2 * m)))
 }
 
-var_m <- function(m = 0) {
-  if (m == 0) {
-    return(1)
-  } else {
-    return(double_factorial(2 * m - 1) / sqrt(4 * m + 1))
-  }
-}
+# var_m <- function(m) {
+#   return(exp(4 * m^2))
+# }
+
 
 var_eps_of_x <- function(x, m = 0) {
   if (length(x) < 2) {
     stop("x must have at least 2 columns")
   }
-  return(exp(- 2 * m * x[, 1]^2) * abs(x[, 2])^(2 * m))
+  # return(exp(- 2 * m * x[, 1]^2) * abs(x[, 2])^(2 * m))
+  return(exp(- 2 * m * abs(x[, 1]) + 2 * m * abs(x[, 2])))
+  # return(exp(- 2 * m * x[, 1] + 2 * m * x[, 2]))  
 }
 
 sim_data_hetero <- function(n = 100, p = 4, s = 1, m = 0) {
