852fa22cb154f3615054f7125fcf212ca92fc411
JesperRVS <jrvs@econ.ku.dk>
2024-07-26 17:23:22 +0200

    added refs

--- lassoVAR.R
+++ lassoVAR.R
@@ -11,14 +11,14 @@
 #'   as in Belloni et al. (2012, ECTA).
 #' @param gamma Tuning parameter for penalty level (score quantile). Default is
 #'   0.1 / log(max(dim(data))), as in Belloni et al. (2012, ECTA).
-#' @param k Maximum number of penalty loading updates. Default is 15 , as in
+#' @param k Maximum number of penalty loading updates. Default is 15, as in
 #'   Belloni et al. (2012, ECTA).
 #' @param tol_ups Convergence tolerance for penalty loadings (relative), so
 #'   that penalty loadings are considered converged if
 #'   ||vec(ups_new - ups_old)||_2 / ||vec(ups_old)||_2 <= tol_ups.
 #' @param warn Logical; if TRUE, issue warning if max updates reached.
-#' @param full_path Logical; if TRUE, use full path of lambda values. Default is
-#'   FALSE.
+#' @param full_path Logical; if TRUE, use full path of lambda values. Default
+#'   is FALSE.
 #' @param tol_glmnet Convergence tolerance for glmnet. (Mainly for debugging.)
 #'
 #' @return A list with the following components:
@@ -38,6 +38,16 @@
 #' \item{rel_diff_ups_term}{Relative change in penalty loadings at termination}
 #' \item{rel_diffs_ups}{Relative changes in penalty loadings along path}
 #'
+#' @references
+#' Belloni, A., Chernozhukov, V., Chen, D., & Hansen, C. (2012).
+#' Sparse models and methods for optimal instruments with an application to 
+#' eminent domain. Econometrica, 80(6), 2369-2429.
+#' \url{https://doi.org/10.3982/ECTA9626}
+#'
+#' Kock, A. B., Pedersen, R. S., & Sørensen, J. R.-V. (2024). Data-Driven Tuning
+#' Parameter Selection for High-Dimensional Vector Autoregressions.
+#' \url{https://arxiv.org/abs/2403.06657}
+#'
 #' @importFrom stats qnorm
 #' @importFrom glmnet glmnet
 #' @importFrom Matrix crossprod
@@ -49,6 +59,23 @@
 #' }
 #'
 #' @export
+source("helper_functions.R")
+lasso_var <- function(data, q = 1, post = TRUE, intercept = TRUE,
+                      c = 1.1, gamma = NULL, k = 15,
+                      tol_ups = 1e-3, warn = TRUE, full_path = FALSE,
+                      tol_glmnet = 1e-4) {
+  # Unpack data and construct predictors and response
+  xy_data <- unpack(data = data, q = q)
+  x <- xy_data$x
+  y <- xy_data$y
+  # Call upon mult_lasso_bcch
+  fit <- mult_lasso_bcch(x = x, y = y, post = post, intercept = intercept,
+                         c = c, gamma = gamma, k = k,
+                         tol_ups = tol_ups, warn = warn, full_path = full_path,
+                         tol_glmnet = tol_glmnet)
+  return(fit)
+}
+
 source("helper_functions.R")
 lasso_var <- function(data, q = 1, post = TRUE, intercept = TRUE,
                       c = 1.1, gamma = NULL, k = 15,
